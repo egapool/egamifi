35234bca8932b700aa002283514b9e62e354edf9
Yusuke Egami <egpersent@gmail.com>
2021-08-08 21:41:16 +0900

    パラメータ追加/シンクを待つように

--- cmd/botbacktest.go
+++ cmd/botbacktest.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/egapool/egamifi/bot"
@@ -40,10 +41,12 @@ func runBacktest() {
 }
 
 func runInago() {
-	fmt.Println("start, end, avg_volume_period, against_avg_volume_rate, minimum_rate, profit, pnl, fee, trade_count, win, lose, total_entry, win_rate, PF")
-	avg_volume_period_list := []int{10, 15, 20}
-	against_avg_volume_rate_list := []float64{5, 10, 15, 20, 25}
-	minimum_rate_list := []float64{1000, 5000}
+	fmt.Println("start, end, avg_volume_period, against_avg_volume_rate, minimum_rate, guard_orver_bb3, minimum_candle_length, profit, pnl, fee, trade_count, win, lose, total_entry, win_rate, PF")
+	avg_volume_period_list := []int{20}
+	against_avg_volume_rate_list := []float64{10, 15, 20}
+	minimum_rate_list := []float64{1000}
+	guard_over_bb3_list := []float64{0.001, 0.003, 0.005}
+	minimum_candle_length_list := []float64{1.5}
 
 	// goroutineで使うためにメモリに読み込み
 	// AXSの場合1日で〜20MB
@@ -65,20 +68,43 @@ func runInago() {
 
 	execute_time := time.Now().Format("2006-01-02-150405")
 
-	ch := make(chan bool, 1)
+	// pt := len(avg_volume_period_list) * len(against_avg_volume_rate_list) * len(minimum_rate_list) * len(guard_over_bb3_list) * len(minimum_candle_length_list)
+	//
+	// var cl int = pt / 6
+	// if cl == 0 {
+	// 	cl = 1
+	// }
+	// fmt.Println(pt, cl)
+
+	var wg sync.WaitGroup
+	ch := make(chan bool, 12)
 	for _, avg_volume_period := range avg_volume_period_list {
 		for _, against_avg_volume_rate := range against_avg_volume_rate_list {
 			for _, minimum_rate := range minimum_rate_list {
-				ch <- true
-				inago_config := inago.NewConfig2(3, avg_volume_period, against_avg_volume_rate, minimum_rate)
-				go exec(ch, trades, inago_config, execute_time)
+				for _, guard_over_bb3 := range guard_over_bb3_list {
+					for _, minimum_candle_length := range minimum_candle_length_list {
+						wg.Add(1)
+						ch <- true
+						inago_config := inago.NewConfig2(
+							3,
+							avg_volume_period,
+							against_avg_volume_rate,
+							minimum_rate,
+							guard_over_bb3,
+							minimum_candle_length,
+						)
+						go exec(ch, &wg, trades, inago_config, execute_time)
+					}
+				}
 			}
 		}
 	}
+	wg.Wait()
 
 }
-func exec(ch chan bool, trades [][]string, inago_config inago.Config, execute_time string) {
+func exec(ch chan bool, wg *sync.WaitGroup, trades [][]string, inago_config inago.Config, execute_time string) {
 	defer func() {
+		wg.Done()
 		<-ch
 	}()
 	logdir := fmt.Sprintf("logs/test/%s/%s/%s", testStrategy, testMarket, execute_time)
